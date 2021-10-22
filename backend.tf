0d42e8f7c171adc53050c28931cb341aaf94920f
yermek-i <56139134+yermek-i@users.noreply.github.com>
2021-10-21 19:08:39 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "jenkins-terraform-evolvecybertraining"
+        bucket = "jenkins-terraform-yermek-training"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
